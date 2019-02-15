71c69c1e3d7a0826fb2ef5e59a973c61dfd0446f
sairandhree <ssule@carbonite.com>
2019-02-15 06:06:29 -0800

    email sender

---
+++ EmailSender.py
@@ -0,0 +1,75 @@
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
+
+def sendEmail(to, attachmentFile, s):
+    today = datetime.now()
+    one_month_ago = today - relativedelta(months = 1)
+    # print "one month ago date time: %s" % one_month_ago
+    last_month_text = one_month_ago.strftime('%B')
+    last_year_full = one_month_ago.strftime('%Y')
+
+
+    print("Salary SLIP for month of {} {} ".format(last_month_text,last_year_full) )
+  
+
+    toaddr = "sairandhree.sule+tab1@niyuj.com"
+    
+    # instance of MIMEMultipart 
+    msg = MIMEMultipart() 
+    
+    # storing the senders email address   
+    msg['From'] = "Niyuj Finance"
+    
+    # storing the receivers email address  
+    msg['To'] = to 
+    
+    # storing the subject  
+    msg['Subject'] = "Salary SLIP for month of {} {} ".format(last_month_text,last_year_full)
+    
+    # string to store the body of the mail 
+    body = ''' <div>Hi,</div>
+
+    <div> Attached is your password protected salary slip for the month of {} {} and the password to open the file is your <strong> PAN in uppercase.</strong> </div>
+    <br/>
+    <div> <font color="red"> Note :  Please make sure that the net amount payable in your salary slip is the same as the sum of the amount credited into your bank account and food card.
+    </font></div>
+    <br/>
+    <div>Thanks and Regards,</div>
+
+    <div>Niyuj Finance</div>'''.format(last_month_text,last_year_full)
+    
+    # attach the body with the msg instance 
+    msg.attach(MIMEText(body, 'html', 'utf-8')) 
+    
+    # open the file to be sent  
+    filename = attachmentFile.split("\\")[-1:][0]
+    attachment = open(attachmentFile, "rb") 
+    
+    # instance of MIMEBase and named as p 
+    p = MIMEBase('application', 'octet-stream') 
+    
+    # To change the payload into encoded form 
+    p.set_payload((attachment).read()) 
+    
+    # encode into base64 
+    encoders.encode_base64(p) 
+    
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+    
+    # attach the instance 'p' to instance 'msg' 
+    msg.attach(p) 
+  
+ 
+    # Converts the Multipart msg into a string 
+    text = msg.as_string() 
+    
+    # sending the mail 
+    s.sendmail("Niyuj Finance", toaddr, text) 
+    
+    
--- sendEmail.py
+++ sendEmail.py
@@ -1,27 +1,86 @@
 import test
 import time
-import smtplib 
+import smtplib
+import os
+import PyPDF2
+import glob
+import shutil
+import sys
+import win32com.client 
 
-start_time = time.time()
 
-fromaddr = "sairandhree.sule@niyuj.com"
-# creates SMTP session 
-s = smtplib.SMTP('smtp.gmail.com', 587) 
+bookName = "Salaries.xlsx"
+masterSheetName = "Master"
+passwordColumn = "B"
+emailColumn = "C"
 
-# start TLS for security 
-s.starttls() 
 
-# Authentication 
-s.login(fromaddr, "Temp1234") 
+def getEmailId(empName, doc):
+    global bookName, masterSheetName, passwordColumn
+    try:
+        sheet = doc.Worksheets(masterSheetName)
+        
+        matchingCell = sheet.UsedRange.Find(empName)
+        
+        passwordCell = "{}{}".format(passwordColumn,matchingCell.Address[-1:])
+        
+        
+        password = sheet.Range(passwordCell)
+       
+        return str(password)
+    except Exception as e:
+        return ""
+
+
+
+def main():
+    global bookName, masterSheetName, passwordColumn
+    if len(sys.argv) != 4:
+        print("Usage:  py PasswordProtectPDFs.py Salaries.xlsx Master D ")
+        sys.exit(1)
+
+    bookName = sys.argv[1]
+    masterSheetName = sys.argv[2]
+    passwordColumn = sys.argv[3]
+
+    path = os.getcwd().replace('\'', '\\') + '\\'
+    excel = win32com.client.gencache.EnsureDispatch('Excel.Application')
+    doc = excel.Workbooks.Open(path+bookName, ReadOnly=True)
+    excel.Visible = False
     
     
+
+
+    fromaddr = "sairandhree.sule@niyuj.com"
+    # creates SMTP session 
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+    # start TLS for security 
+    s.starttls() 
+
+    # Authentication 
+    s.login(fromaddr, "Ananya12") 
     
-   
-test.sendEmail("sairandhree.sule+a@niyuj.com", "protected/a.txt", s)
-test.sendEmail("sairandhree.sule+b@niyuj.com", "protected/b.txt", s)
+    pdfFiles = glob.glob("protected"+'\*.pdf')
 
- # terminating the session 
-s.quit()
+    print(pdfFiles,path)
+    for file in pdfFiles:
+        empName = file[:-4]
+        empName = empName.split("\\")[-1:][0]
+        print(empName)
+        
+        email = getEmailId(empName,doc)
+        print("sending {} to {}".format(file,email))
+        test.sendEmail(email, file, s)
+
+    
+    doc.Close(SaveChanges=False)
+    excel.Quit()
 
 
-print("--- %s seconds ---" % (time.time() - start_time))
+    # terminating the session
+    s.quit()
+
+
+if __name__ == "__main__":
+    main()
